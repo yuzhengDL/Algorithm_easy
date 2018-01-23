e3b8eed6522f6b4e01f5999cdc7d07c7fb533919
yuzhengDL <18811435811@163.com>
2018-01-23 20:30:10 +0800

    Update Two_sum.py

--- Two_sum.py
+++ Two_sum.py
@@ -41,6 +41,6 @@ class Solution:
         for i in range(len(nums)):
             x = nums[i]
             if target-x in dict:
-                return (dict[target-x]+1, i+1)
+                return (dict[target-x], i)
             dict[x] = i
 */
