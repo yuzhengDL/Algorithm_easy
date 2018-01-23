bab8a20e8f1065e821eaf40642c6c6a7b71f8950
yuzhengDL <18811435811@163.com>
2018-01-23 22:03:36 +0800

    Update Valid_Sudoku.py

--- Valid_Sudoku.py
+++ Valid_Sudoku.py
@@ -1,5 +1,11 @@
 /*
-参考博客https://www.cnblogs.com/breada/p/4924092.html.
+参考博客https://www.cnblogs.com/breada/p/4924092.html
+数独规则：1.任意行只包含1-9不重复的数字；2.任意列只包含1-9不重复的数字；3.任意宫（3X3）只包含1-9不重复的数字
+一些注意事项：
+1.遍历得到list，使用列表解析式 unit = [i for i in s if i != '.']
+2.减少变量的使用
+3.zip变量
+4.对列表的操作，可以减少append等操作，选择+
 */
 
 
