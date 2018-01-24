e604f907ad9365638de4c4174113d1fd7d2c064e
yuzhengDL <18811435811@163.com>
2018-01-24 19:51:03 +0800

    Update Reverse_String.py

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,3 +1,6 @@
+/*
+思路：相当于对字符串做轴对称变换，即设置一前一后两个指针，交换当前指针指向的数据即可。
+*/
 class Solution(object):
     def reverseString(self, s):
         """
@@ -13,7 +16,8 @@ class Solution(object):
             i += 1
             j -= 1
         return ''.join(res)
-        
+
+/*python中的简洁实现
 class SolutionPythonic(object):
     def reverseString(self, s):
         return s[::-1]
