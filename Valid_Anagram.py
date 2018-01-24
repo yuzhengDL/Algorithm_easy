ab31f7677c6c3610aeb15fbdea5417f13de285c6
yuzhengDL <18811435811@163.com>
2018-01-24 15:26:45 +0800

    Update Valid_Anagram.py

--- Valid_Anagram.py
+++ Valid_Anagram.py
@@ -15,7 +15,9 @@ class Solution(object):
         return Counter(s) == Counter(t)
         
 /*
-思路：先排序，后比较。
+思路：先排序，后比较。sorted()函数对所有可迭代的对象进行排序操作。其与sort()的区别是：
+sort 是应用在 list 上的方法，sorted 可以对所有可迭代的对象进行排序操作。
+list 的 sort 方法返回的是对已经存在的列表进行操作，而内建函数 sorted 方法返回的是一个新的 list，而不是在原来的基础上进行的操作。
 */
 from collections import Counter
 
