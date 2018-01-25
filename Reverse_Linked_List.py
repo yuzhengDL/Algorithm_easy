3b6670dea3922fcc0b303d3df2f90623a2f388d7
yuzhengDL <18811435811@163.com>
2018-01-25 11:34:48 +0800

    Create Reverse_Linked_List.py

---
+++ Reverse_Linked_List.py
@@ -0,0 +1,25 @@
+/*
+思路：迭代，指定一前一后两个指针，通过中间变脸temp实现链表的反转。
+*/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        slow = None
+        while head:
+            curr = head
+            head = head.next
+            temp = slow
+            slow = curr
+            curr.next = temp
+        return slow
