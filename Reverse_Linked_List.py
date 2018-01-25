f65dc6b7d1571e6e5631b7d815f7a69ba8bdf448
yuzhengDL <18811435811@163.com>
2018-01-25 15:25:48 +0800

    Update Reverse_Linked_List.py

--- Reverse_Linked_List.py
+++ Reverse_Linked_List.py
@@ -23,3 +23,28 @@ class Solution(object):
             slow = curr
             curr.next = temp
         return slow
+
+
+/*
+思路：递归。
+*/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return head
+        
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
