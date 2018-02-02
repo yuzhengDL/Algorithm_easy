1a2c3705085c4b3cd8f76bcf9cc34834bb32a903
yuzhengDL <18811435811@163.com>
2018-02-02 11:59:16 +0800

    Update Fizz_Buzz.py

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,3 +1,28 @@
+/*
+问题描述：
+输入n，输出1到n数字的字符串描述。如果数字是3的倍数，则输出换成'Fizz'；若数字是5的倍数，则输出换成'Buzz'；若数字即是三的倍数也是五的倍数，则输出'FizzBuzz'.
+
+n = 15,
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+*/
+
 class Solution(object):
     def fizzBuzz(self, n):
         """
